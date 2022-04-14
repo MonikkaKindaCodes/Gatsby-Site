79c41bd100b16796b1e99a382b36765a378ea947
Monikka <59119237+MonikkaKindaCodes@users.noreply.github.com>
2022-04-14 12:01:21 -0500

    Add files via upload

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,9 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Fullstack Gatsby!",
+    description: "Monikka Edgeston",
+  },
   plugins: ["gatsby-plugin-sass"],
-}
+};
